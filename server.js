d8802f87d76eee973ebdb162d936d04ec51aac74
Azamantes <azamantes@protonmail.com>
2016-09-01 19:38:15 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const data = JSON.stringify({
+    id: 2,
+    name: 'Mateusz',
+});
+
+const server = http.createServer((request, response) => {
+    response.end(data);
+});
+server.listen(80);
