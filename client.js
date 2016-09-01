84250cec5222b2bfa8c749353452b30c2e1ee930
Azamantes <azamantes@protonmail.com>
2016-09-01 20:36:40 +0100

    Update client.js

--- client.js
+++ client.js
@@ -1,34 +1,14 @@
 const http = require('http');
 
 const config = {
-    host: '192.168.85.10',
-    path: '/'
+    host: '192.168.5.10',
+    post: 8080,
+    path: '/',
+    method: 'GET',
+    agent: 'false',
 };
 
-let connections = 0;
-
-
-request();
-
-setInterval(() => {
-    console.log('Connections:', connections);
-    connections = 0;
-}, 1000);
-
 function request() {
-    http.get(config, response => {
-        let body = '';
-        response.on('dta', data => {
-            body += data;
-        });
-        response.on('end', () => {
-            const parsed = JSON.parse(body);
-            const isValid = +parsed.id > 0 && typeof parser.name === 'string';
-            if(isValid) {
-                connections += 1;
-            }
-
-            setTimeout(request);
-        });
-    });
+    http.get(config, handler);
+    return request();
 }
