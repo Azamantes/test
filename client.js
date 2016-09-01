9a3b2a8356069567ad03dd48415a878a229430e9
Azamantes <azamantes@protonmail.com>
2016-09-01 20:56:22 +0100

    Update client.js

--- client.js
+++ client.js
@@ -3,6 +3,6 @@ const http = require('http');
 (function request() {
     http.get('http://192.168.5.10:8080/', response => {
         response.on('data', () => {})
-        response.on('end', () => request);
+        response.on('end', () => request());
     });
 }());
